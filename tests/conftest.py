cea30d63ce80f8e034b105edbe467b58215c8b22
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-09-29 16:58:19 -0400

    Try some env vars

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,6 @@ from threading import Thread
 from bot import API
 
 
-@pytest.fixture(autouse=True)
-def set_fake_env_vars(monkeypatch):
-    """Sets fake environmental variables for Slack"""
-    monkeypatch.setattr(os.environ, "get", lambda key: "Fake value")
-
-
 @pytest.fixture
 def test_client():
     """Returns a Starlette test API instance"""
